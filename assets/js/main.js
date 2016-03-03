72455c3beb8f152d5e7f3292dc9e4f3f6e994b78
Soheil <Damangir@users.noreply.github.com>
2016-03-03 04:42:25 +0100

    content and redesign

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,24 @@
 //Contact Form
 
 $('#submit').click(function(){
-
 $.post("assets/php/send.php", $(".contact-form").serialize(),  function(response) {   
  $('#success').html(response);
 });
 return false;
+});
 
+var aspect_ratio = 0.45;
+
+// Store the jQuery object for future reference
+var $my_div = $("#hero-area");
+
+// Within your document ready function,
+// Do an initial resize of #my_div
+$(document).ready(function(){
+  $my_div.css("min-height", $my_div.width() * aspect_ratio );
+});
+
+// Resize #my_div on browser resize
+$(window).resize(function() {
+  $my_div.css("min-height", $my_div.width() * aspect_ratio );
 });
